f35dd8d612beeb68bf4fe9d092476bb3cc03f002
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-08 19:43:23 +0500

    fix typo

--- Assigment-No-1/fabonacci-using-loop.js
+++ Assigment-No-1/fabonacci-using-loop.js
@@ -3,12 +3,12 @@
 // the elements of the array are calculated as fabonnaci sequence
 // e.g fib(8) returns [0, 1, 1,  2,  3, 5, 8, 13]
 
-function fib(number) {
+function fib(len) {
 	let arr = [0, 1];
-	if (number <= 0) return [];
-	if (number == 1) return [0];
-	if (number == 2) return arr;
-	for (let i = 2; i < number; i++) {
+	if (len <= 0) return [];
+	if (len == 1) return [0];
+	if (len == 2) return arr;
+	for (let i = 2; i < len; i++) {
 		arr.push(arr[arr.length - 2] + arr[arr.length - 1]);
 	}
 	return arr;
